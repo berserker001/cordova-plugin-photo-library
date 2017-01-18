736e925ee8061305f91b18ef66fb3b222fc71735
Exia <648503411@qq.com>
2017-01-18 12:56:40 +0800

    Exia 0.0.1 1.Support to read video list. 2.Support to sort library items by timestamp.

--- www/PhotoLibrary.js
+++ www/PhotoLibrary.js
@@ -12,36 +12,42 @@ var photoLibrary = {};
 // Will start caching for specified size
 photoLibrary.getLibrary = function (success, error, options, partialCallback) {
 
-  if (!options) {
-    options = {};
-  }
+    if (!options) {
+        options = {};
+    }
 
-  options = {
-    thumbnailWidth: options.thumbnailWidth || defaultThumbnailWidth,
-    thumbnailHeight: options.thumbnailHeight || defaultThumbnailHeight,
-    quality: options.quality || defaultQuality,
-  };
+    options = {
+        thumbnailWidth: options.thumbnailWidth || defaultThumbnailWidth,
+        thumbnailHeight: options.thumbnailHeight || defaultThumbnailHeight,
+        quality: options.quality || defaultQuality
+    };
 
-  cordova.exec(
-    function (result) {
+    cordova.exec(
+        function (result) {
 
-      var library = result.library;
-      var isPartial = result.isPartial;
+            var library = result.library;
+            var isPartial = result.isPartial;
 
-      if (isPartial) {
-        if (typeof partialCallback === 'function') {
-          addUrlsToLibrary(library, partialCallback, options);
-        }
-        return;
-      }
+            //Do not need to support partial callback.
 
-      addUrlsToLibrary(library, success, options);
+            // if (isPartial) {
+            //     if (typeof partialCallback === 'function') {
+            //         addUrlsToLibraryItem(library, partialCallback, options);
+            //     }
+            //     return;
+            // }
 
-    },
-    error,
-    'PhotoLibrary',
-    'getLibrary', [options]
-  );
+            addUrlsToLibraryItem(library, options);
+
+            library.sort(function (itemA, itemB) {
+                return itemA.timestamp > itemB.timestamp;
+            });
+
+        },
+        error,
+        'PhotoLibrary',
+        'getLibrary', [options]
+    );
 
 };
 
@@ -49,29 +55,66 @@ photoLibrary.getLibrary = function (success, error, options, partialCallback) {
 // If success callback not provided, will return value immediately, but use overload with success as it browser-friendly
 photoLibrary.getThumbnailURL = function (photoIdOrLibraryItem, success, error, options) {
 
-  var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
+    var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
 
-  if (typeof success !== 'function' && typeof options === 'undefined') {
-    options = success;
-    success = undefined;
-  }
-
-  options = getThumbnailOptionsWithDefaults(options);
-
-  var thumbnailURL = 'cdvphotolibrary://thumbnail?photoId=' + fixedEncodeURIComponent(photoId) +
-    '&width=' + fixedEncodeURIComponent(options.thumbnailWidth) +
-    '&height=' + fixedEncodeURIComponent(options.thumbnailHeight) +
-    '&quality=' + fixedEncodeURIComponent(options.quality);
-
-  if (success) {
-    if (isBrowser) {
-      cordova.exec(success, error, 'PhotoLibrary', '_getThumbnailURLBrowser', [photoId, options]);
-    } else {
-      success(thumbnailURL);
+    if (typeof success !== 'function' && typeof options === 'undefined') {
+        options = success;
+        success = undefined;
     }
-  } else {
-    return thumbnailURL;
-  }
+
+    options = getThumbnailOptionsWithDefaults(options);
+
+    var thumbnailURL = 'cdvphotolibrary://thumbnail?photoId=' + fixedEncodeURIComponent(photoId) +
+        '&width=' + fixedEncodeURIComponent(options.thumbnailWidth) +
+        '&height=' + fixedEncodeURIComponent(options.thumbnailHeight) +
+        '&quality=' + fixedEncodeURIComponent(options.quality);
+
+    if (success) {
+        if (isBrowser) {
+            cordova.exec(success, error, 'PhotoLibrary', '_getThumbnailURLBrowser', [photoId, options]);
+        } else {
+            success(thumbnailURL);
+        }
+    } else {
+        return thumbnailURL;
+    }
+
+};
+
+photoLibrary.getVideoThumbnailURL = function (libraryItem, success, error, options) {
+
+    if (typeof success !== 'function' && typeof options === 'undefined') {
+        options = success;
+        success = undefined;
+    }
+
+    options = getThumbnailOptionsWithDefaults(options);
+
+    var thumbnailURL = 'cdvphotolibrary://video?videoId=' + fixedEncodeURIComponent(libraryItem.nativeURL) +
+        '&width=' + fixedEncodeURIComponent(options.thumbnailWidth) +
+        '&height=' + fixedEncodeURIComponent(options.thumbnailHeight);
+
+    if (success) {
+        if (isBrowser) {
+            cordova.exec(success, success, 'PhotoLibrary', '_getPhotoURLBrowser', [photoId, options]);
+        } else {
+            success(photoURL);
+        }
+        success(thumbnailURL);
+    } else {
+        return thumbnailURL;
+    }
+};
+
+photoLibrary.getVideoPosterURL = function (libraryItem, success) {
+
+    var posterURL = 'cdvphotolibrary://videoPoster?videoId=' + fixedEncodeURIComponent(libraryItem.nativeURL);
+
+    if (success) {
+        success(posterURL);
+    } else {
+        return posterURL;
+    }
 
 };
 
@@ -79,131 +122,131 @@ photoLibrary.getThumbnailURL = function (photoIdOrLibraryItem, success, error, o
 // If success callback not provided, will return value immediately, but use overload with success as it browser-friendly
 photoLibrary.getPhotoURL = function (photoIdOrLibraryItem, success, error, options) {
 
-  var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
+    var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
 
-  if (typeof success !== 'function' && typeof options === 'undefined') {
-    options = success;
-    success = undefined;
-  }
-
-  if (!options) {
-    options = {};
-  }
-
-  var photoURL = 'cdvphotolibrary://photo?photoId=' + fixedEncodeURIComponent(photoId);
-
-  if (success) {
-    if (isBrowser) {
-      cordova.exec(success, success, 'PhotoLibrary', '_getPhotoURLBrowser', [photoId, options]);
-    } else {
-      success(photoURL);
+    if (typeof success !== 'function' && typeof options === 'undefined') {
+        options = success;
+        success = undefined;
     }
-  } else {
-    return photoURL;
-  }
+
+    if (!options) {
+        options = {};
+    }
+
+    var photoURL = 'cdvphotolibrary://photo?photoId=' + fixedEncodeURIComponent(photoId);
+
+    if (success) {
+        if (isBrowser) {
+            cordova.exec(success, success, 'PhotoLibrary', '_getPhotoURLBrowser', [photoId, options]);
+        } else {
+            success(photoURL);
+        }
+    } else {
+        return photoURL;
+    }
 
 };
 
 // Provide same size as when calling getLibrary for better performance
 photoLibrary.getThumbnail = function (photoIdOrLibraryItem, success, error, options) {
 
-  var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
+    var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
 
-  options = getThumbnailOptionsWithDefaults(options);
+    options = getThumbnailOptionsWithDefaults(options);
 
-  cordova.exec(
-    function (data, mimeType) {
-      if (!mimeType && data.data && data.mimeType) {
-        // workaround for browser platform cannot return multipart result
-        mimeType = data.mimeType;
-        data = data.data;
-      }
-      var blob = new Blob([data], {
-        type: mimeType
-      });
-      success(blob);
-    },
-    error,
-    'PhotoLibrary',
-    'getThumbnail', [photoId, options]
-  );
+    cordova.exec(
+        function (data, mimeType) {
+            if (!mimeType && data.data && data.mimeType) {
+                // workaround for browser platform cannot return multipart result
+                mimeType = data.mimeType;
+                data = data.data;
+            }
+            var blob = new Blob([data], {
+                type: mimeType
+            });
+            success(blob);
+        },
+        error,
+        'PhotoLibrary',
+        'getThumbnail', [photoId, options]
+    );
 
 };
 
 photoLibrary.getPhoto = function (photoIdOrLibraryItem, success, error, options) {
 
-  var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
+    var photoId = typeof photoIdOrLibraryItem.id !== 'undefined' ? photoIdOrLibraryItem.id : photoIdOrLibraryItem;
 
-  if (!options) {
-    options = {};
-  }
+    if (!options) {
+        options = {};
+    }
 
-  cordova.exec(
-    function (data, mimeType) {
-      if (!mimeType && data.data && data.mimeType) {
-        // workaround for browser platform cannot return multipart result
-        mimeType = data.mimeType;
-        data = data.data;
-      }
-      var blob = new Blob([data], {
-        type: mimeType
-      });
-      success(blob);
-    },
-    error,
-    'PhotoLibrary',
-    'getPhoto', [photoId, options]
-  );
+    cordova.exec(
+        function (data, mimeType) {
+            if (!mimeType && data.data && data.mimeType) {
+                // workaround for browser platform cannot return multipart result
+                mimeType = data.mimeType;
+                data = data.data;
+            }
+            var blob = new Blob([data], {
+                type: mimeType
+            });
+            success(blob);
+        },
+        error,
+        'PhotoLibrary',
+        'getPhoto', [photoId, options]
+    );
 
 };
 
 // Call when thumbnails are not longer needed for better performance
 photoLibrary.stopCaching = function (success, error) {
 
-  cordova.exec(
-    success,
-    error,
-    'PhotoLibrary',
-    'stopCaching', []
-  );
+    cordova.exec(
+        success,
+        error,
+        'PhotoLibrary',
+        'stopCaching', []
+    );
 
 };
 
 // Call when getting errors that begin with 'Permission Denial'
 photoLibrary.requestAuthorization = function (success, error, options) {
 
-  options = getRequestAuthenticationOptionsWithDefaults(options);
+    options = getRequestAuthenticationOptionsWithDefaults(options);
 
-  cordova.exec(
-    success,
-    error,
-    'PhotoLibrary',
-    'requestAuthorization', [options]
-  );
+    cordova.exec(
+        success,
+        error,
+        'PhotoLibrary',
+        'requestAuthorization', [options]
+    );
 
 };
 
 // url is file url or dataURL
 photoLibrary.saveImage = function (url, album, success, error) {
 
-  cordova.exec(
-    success,
-    error,
-    'PhotoLibrary',
-    'saveImage', [url, album]
-  );
+    cordova.exec(
+        success,
+        error,
+        'PhotoLibrary',
+        'saveImage', [url, album]
+    );
 
 };
 
 // url is file url or dataURL
 photoLibrary.saveVideo = function (url, album, success, error) {
 
-  cordova.exec(
-    success,
-    error,
-    'PhotoLibrary',
-    'saveVideo', [url, album]
-  );
+    cordova.exec(
+        success,
+        error,
+        'PhotoLibrary',
+        'saveVideo', [url, album]
+    );
 
 };
 
@@ -211,65 +254,63 @@ module.exports = photoLibrary;
 
 var getThumbnailOptionsWithDefaults = function (options) {
 
-  if (!options) {
-    options = {};
-  }
+    if (!options) {
+        options = {};
+    }
 
-  options = {
-    thumbnailWidth: options.thumbnailWidth || defaultThumbnailWidth,
-    thumbnailHeight: options.thumbnailHeight || defaultThumbnailHeight,
-    quality: options.quality || defaultQuality,
-  };
+    options = {
+        thumbnailWidth: options.thumbnailWidth || defaultThumbnailWidth,
+        thumbnailHeight: options.thumbnailHeight || defaultThumbnailHeight,
+        quality: options.quality || defaultQuality,
+    };
 
-  return options;
+    return options;
 
 };
 
 var getRequestAuthenticationOptionsWithDefaults = function (options) {
 
-  if (!options) {
-    options = {};
-  }
+    if (!options) {
+        options = {};
+    }
 
-  options = {
-    read: options.read || true,
-    write: options.write || false,
-  };
+    options = {
+        read: options.read || true,
+        write: options.write || false,
+    };
 
-  return options;
+    return options;
 
 };
 
-var addUrlsToLibrary = function (library, callback, options) {
+var addUrlsToLibraryItem = function (library, options) {
 
-  var urlsLeft = library.length;
+    for (var i = 0; i < library.length; i++) {
 
-  var handlePhotoURL = function (libraryItem, photoURL) {
-    libraryItem.photoURL = photoURL;
-    urlsLeft -= 1;
-    if (urlsLeft === 0) {
-      callback(library);
+        var libraryItem = library[i];
+
+        if (libraryItem.timestamp != null)
+            libraryItem.timestamp = Math.abs(libraryItem.timestamp);
+
+        if (libraryItem.duration != null) {
+            libraryItem.isVideo = true;
+
+            libraryItem.videoThumbnailURL = photoLibrary.getVideoThumbnailURL(libraryItem, null, null, options);
+            libraryItem.videoPosterURL = photoLibrary.getVideoPosterURL(libraryItem, null);
+        }
+        else {
+
+            libraryItem.thumbnailURL = photoLibrary.getThumbnailURL(libraryItem, null, null, options);
+            libraryItem.photoURL = photoLibrary.getPhotoURL(libraryItem, null, null);
+        }
+
     }
-  };
-
-  var handleThumbnailURL = function (libraryItem, thumbnailURL) {
-    libraryItem.thumbnailURL = thumbnailURL;
-    photoLibrary.getPhotoURL(libraryItem, handlePhotoURL.bind(null, libraryItem), handleUrlError);
-  };
-
-  var handleUrlError = function () {}; // Should never happen
-
-  var i;
-  for (i = 0; i < library.length; i++) {
-    var libraryItem = library[i];
-    photoLibrary.getThumbnailURL(libraryItem, handleThumbnailURL.bind(null, libraryItem), handleUrlError, options);
-  }
 
 };
 
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
 function fixedEncodeURIComponent(str) {
-  return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
-    return '%' + c.charCodeAt(0).toString(16);
-  });
+    return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
+        return '%' + c.charCodeAt(0).toString(16);
+    });
 }
